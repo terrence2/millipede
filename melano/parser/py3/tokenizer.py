00fdc75255d7eb4fe620b7e947b762f955723793
Terrence Cole <terrence@trainedmonkeystudios.org>
2010-05-19 15:20:35 -0700

    wrap and fix-up python3's native tokenizer

---
+++ melano/parser/py3/tokenizer.py
@@ -0,0 +1,30 @@
+'''
+Top-level wrapper for tokenization of python sources.
+'''
+__author__ = 'Terrence Cole <terrence@zettabytestorage.com>'
+
+from io import StringIO
+from tokenize import _tokenize
+
+
+class PythonTokenizer:
+	def __init__(self, grammar):
+		self.grammar = grammar
+
+
+	def tokenize(self, source:str):
+		fp = StringIO(source)
+		tokens = list(self.token_iter(fp.readline))
+		return tokens
+
+
+	def token_iter(self, readline):
+		tokiter = _tokenize(readline, None)
+		opmap = self.grammar.OPERATOR_MAP
+		for tok in tokiter:
+			ttype = opmap.get(tok.string, None)
+			if ttype:
+				yield tok._replace(type=ttype)
+			else:
+				yield tok
+
