2da2d029daf1a2d1134fd708afe3b980e08894f3
Terrence Cole <terrence@trainedmonkeystudios.org>
2010-05-19 12:42:53 -0700

    do actual option parsing

--- melinto.py
+++ melinto.py
@@ -7,15 +7,20 @@ __author__ = 'Terrence Cole <terrence@zettabytestorage.com>'
 
 
 if __name__ == '__main__':
+	import optparse
 	import sys
 	from tokenize import detect_encoding
 
-	filename = sys.argv[1]
-	
-	# read the file contents
-	with open(filename, 'rb') as fp:
-		encoding, _ = detect_encoding(fp.readline)
-	with open(filename, 'rt', encoding=encoding) as fp:
-		content = fp.read()
-	print(content)
+	# parse the command line
+	parser = optparse.OptionParser('melinto')
+	options, args = parser.parse_args()
+
+	# lint each given file
+	for filename in args:
+		# read the file contents
+		with open(filename, 'rb') as fp:
+			encoding, _ = detect_encoding(fp.readline)
+		with open(filename, 'rt', encoding=encoding) as fp:
+			content = fp.read()
+		print(content)
 
