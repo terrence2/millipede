76fabdba3e6aa827e9a979819bbe4419c36209af
Terrence Cole <terrence@trainedmonkeystudios.org>
2010-05-20 14:04:33 -0700

    visitor based ast traversal from CPython

---
+++ melano/parser/common/visitor.py
@@ -0,0 +1,37 @@
+'''
+Walk an AST.  Copied from CPython.
+'''
+from .ast import AST
+
+
+def iter_fields(node):
+	"""
+	Yield a tuple of ``(fieldname, value)`` for each field in ``node._fields``
+	that is present on *node*.
+	"""
+	for field in node._fields:
+		try:
+			yield field, getattr(node, field)
+		except AttributeError:
+			print("No attribute: {} on {}".format(field, node.__class__.__name__))
+
+
+
+class ASTVisitor:
+	def visit(self, node):
+		"""Visit a node."""
+		method = 'visit_' + node.__class__.__name__
+		visitor = getattr(self, method, self.generic_visit)
+		return visitor(node)
+
+
+	def generic_visit(self, node):
+		"""Called if no explicit visitor function exists for a node."""
+		for field, value in iter_fields(node):
+			if isinstance(value, list):
+				for item in value:
+					if isinstance(item, AST):
+						self.visit(item)
+			elif isinstance(value, AST):
+				self.visit(value)
+
