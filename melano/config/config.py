274a45af8b5a9af363879c1bdd6e6c79ffe2b532
Terrence Cole <terrence@trainedmonkeystudios.org>
2010-05-26 14:20:59 -0700

    this is the wrong install directory

--- melano/config/config.py
+++ melano/config/config.py
@@ -42,7 +42,7 @@ class MelanoConfig:
 			os.makedirs(self.cache_dir, 0o755)
 
 		# find our application data
-		self.data_dir = '/usr/share/melinto-{}/'.format(VERSION)
+		self.data_dir = '/usr/share/melano-{}/'.format(VERSION)
 		if os.path.exists('./data/grammar/python-3.1'):
 			self.data_dir = os.path.join(os.path.realpath('.'), 'data')
 
