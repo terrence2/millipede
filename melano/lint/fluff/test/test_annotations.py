8eef56f4555bed4219d9896d05c17f74a06743ed
Terrence Cole <terrence@trainedmonkeystudios.org>
2010-06-04 18:19:53 -0700

    add another test for annotations

--- melano/lint/fluff/test/test_annotations.py
+++ melano/lint/fluff/test/test_annotations.py
@@ -10,7 +10,7 @@ from melano.lint.message import C0113, C0114
 
 
 class TestLintAnnotations(FluffTestBase):
-	def test_annotations(self):
+	def test_no_annotations(self):
 		prog = '''
 def foo(a, b, c):
 	return 'foo'
@@ -20,4 +20,13 @@ def foo(a, b, c):
 		self.assertEqual(len(messages), 4)
 		self.assertSameElements([C0113]*3 + [C0114], [m.__class__ for m in messages])
 
+
+	def test_full_annotations(self):
+		prog = '''
+def foo(a:int, b:int, c:int) -> str:
+	return 'foo'
+'''
+		with self.create('ann', prog) as unit:
+			messages = list(analyse(unit))
+		self.assertEqual(len(messages), 0)
 
