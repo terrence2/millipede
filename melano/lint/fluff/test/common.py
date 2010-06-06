d217534b765b9ca1b703de9abfd5caa9cd633765
Terrence Cole <terrence@trainedmonkeystudios.org>
2010-06-05 18:09:53 -0700

    another layer of abstraction for easy use of units

--- melano/lint/fluff/test/common.py
+++ melano/lint/fluff/test/common.py
@@ -3,26 +3,9 @@ Base test setup, teardown, and utilities for all fluff tests.
 '''
 __author__ = 'Terrence Cole <terrence@zettabytestorage.com>'
 
-from melano.code.unit import MelanoCodeUnit
-from melano.config.config import MelanoConfig
-from melano.test.filebased import FileBasedTest
-from contextlib import contextmanager
-import os
+from melano.test.unitbased import UnitBasedTest
 
 
-class FluffTestBase(FileBasedTest):
-	def setUp(self):
-		self.config = MelanoConfig()
-
-
-	def tearDown(self):
-		for fn in os.listdir('/tmp'):
-			if fn.startswith('melinto-'):
-				os.unlink('/tmp/' + fn)
-	
-
-	@contextmanager
-	def create(self, name, code):
-		with super().create(name, code) as fn:
-			yield MelanoCodeUnit(self.config, fn)
+class FluffTestBase(UnitBasedTest):
+	pass
 
