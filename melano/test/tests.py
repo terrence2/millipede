3636b149d2ffa11d3b6a44870fdd57d525fed995
Terrence Cole <terrence@trainedmonkeystudios.org>
2010-07-22 22:04:51 -0700

    sort the test collection

--- melano/test/tests.py
+++ melano/test/tests.py
@@ -5,10 +5,11 @@ __author__ = 'Terrence Cole <terrence@zettabytestorage.com>'
 
 from melano.parser.test.test_parse import TestParse
 from melano.lint.fluff.test.test_annotations import TestLintAnnotations
-from melano.lint.fluff.test.test_docstrings import TestLintDocstrings
-from melano.lint.fluff.test.test_methodargs import TestLintMethodArgs
 from melano.lint.fluff.test.test_classinit import TestLintClassInit
+from melano.lint.fluff.test.test_docstrings import TestLintDocstrings
 from melano.lint.fluff.test.test_generator import TestLintGenerator
-from melano.lint.fluff.test.test_returns import TestLintReturns
 from melano.lint.fluff.test.test_imports import TestLintImports
+from melano.lint.fluff.test.test_methodargs import TestLintMethodArgs
+from melano.lint.fluff.test.test_name_use import TestLintNameUse
+from melano.lint.fluff.test.test_returns import TestLintReturns
 
