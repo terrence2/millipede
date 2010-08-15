44091e646f8bee812ab687e19877dd5f8da022a0
Terrence Cole <terrence@trainedmonkeystudios.org>
2010-08-15 13:32:20 -0700

    add new names for messages we want to support

--- melano/lint/messages.py
+++ melano/lint/messages.py
@@ -62,3 +62,71 @@ class W0401(Message):
 class W0410(Message):
 	'''__future__ import is not the first non docstring statement'''
 
+
+class W0611(Message):
+	'''Unused import {}'''
+
+class W0612(Message):
+	'''Unused variable {}'''
+
+class W0613(Message):
+	'''Unused argument {}'''
+
+class W0615(Message):
+	'''Unused optional variable in with statement'''
+
+class E0601(Message):
+	'''Using variable {} before assignment'''
+
+class E0602(Message):
+	'''Undefined variable {}'''
+
+class E0603(Message):
+	'''Used variable {} after unbound'''
+
+
+## todo
+class W0621(Message):
+	'''Redefining name {} from outer scope (line {})'''
+
+class W0622(Message):
+	'''Redefining built-in {}'''
+
+class W0631(Message):
+	'''Using possibly undefined loop variable {}'''
+
+
+
+## Need full-program linting for this
+class W0614(Message):
+	'''Unused import {} from wildcard import'''
+
+
+#### PyFlakes Errors
+#13 W0611	UnusedImport(Message):
+#	W0612	UnusedAssignment
+#	W0613	UnusedArgument
+#27 	class ImportShadowedByLoopVar(Message):
+#			NameShadowedByLoopVar
+#41 	class UndefinedName(Message):
+#49 	class UndefinedExport(Message):
+#57 	class UndefinedLocal(Message):
+#64 	class DuplicateArgument(Message):
+#71 	class RedefinedFunction(Message):
+#20 	class RedefinedWhileUnused(Message):
+#85 	class UnusedVariable(Message):
+
+class W0702(Message):
+	'''No exception type(s) specified'''
+
+
+#import time
+#import f00
+#def foo():
+#	if time.time() % 256 == f00.bar():
+#		a = time.time()
+#	def bar():
+#		import b4r
+#		return b4r.bar(a)
+#foo()()
+
