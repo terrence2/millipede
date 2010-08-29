dc2343f4f2e18c6bf5f4aabb0051061b2db8e1db
Terrence Cole <terrence@zettabytestorage.com>
2010-08-29 14:24:16 -0700

    set unique object names on our dock widgets

--- melano/ui/main.py
+++ melano/ui/main.py
@@ -34,12 +34,14 @@ class MelanoMainWindow(QMainWindow):
 
 		self.projectsList = MelanoProjectListWidget(self)
 		self.dockProjectList = QDockWidget("Project List", self)
+		self.dockProjectList.setObjectName('dockProjectList')
 		self.dockProjectList.setAllowedAreas(QtCore.Qt.LeftDockWidgetArea | QtCore.Qt.RightDockWidgetArea)
 		self.dockProjectList.setWidget(self.projectsList)
 		self.addDockWidget(QtCore.Qt.LeftDockWidgetArea, self.dockProjectList)
 
 		self.projectBrowser = MelanoProjectTreeWidget(self)
 		self.dockProjectTree = QDockWidget("Project Browser", self);
+		self.dockProjectTree.setObjectName('dockProjectTree')
 		self.dockProjectTree.setAllowedAreas(QtCore.Qt.LeftDockWidgetArea | QtCore.Qt.RightDockWidgetArea);
 		self.dockProjectTree.setWidget(self.projectBrowser)
 		self.addDockWidget(QtCore.Qt.LeftDockWidgetArea, self.dockProjectTree)
