17db2521b27c1f3b2f65a55b4af170e4d7b43625
Terrence Cole <terrence@trainedmonkeystudios.org>
2011-01-16 11:41:22 -0800

    these are no longer relevant

--- melano/test/tests.py
+++ melano/test/tests.py
@@ -2,13 +2,3 @@
 Unittest needs to be spoon-fed the list of tests.
 '''
 __author__ = 'Terrence Cole <terrence@zettabytestorage.com>'
-
-from melano.lint.fluff.test.test_annotations import TestLintAnnotations
-from melano.lint.fluff.test.test_classinit import TestLintClassInit
-from melano.lint.fluff.test.test_docstrings import TestLintDocstrings
-from melano.lint.fluff.test.test_generator import TestLintGenerator
-from melano.lint.fluff.test.test_imports import TestLintImports
-from melano.lint.fluff.test.test_methodargs import TestLintMethodArgs
-from melano.lint.fluff.test.test_name_use import TestLintNameUse
-from melano.lint.fluff.test.test_returns import TestLintReturns
-
