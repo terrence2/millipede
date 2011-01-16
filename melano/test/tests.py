94c04e4a1057cac6534654bb2469fdef24c42961
Terrence Cole <terrence@trainedmonkeystudios.org>
2011-01-16 11:39:51 -0800

    this is hidously inaddequate and completely broken now

--- melano/test/tests.py
+++ melano/test/tests.py
@@ -3,7 +3,6 @@ Unittest needs to be spoon-fed the list of tests.
 '''
 __author__ = 'Terrence Cole <terrence@zettabytestorage.com>'
 
-from melano.parser.test.test_parse import TestParse
 from melano.lint.fluff.test.test_annotations import TestLintAnnotations
 from melano.lint.fluff.test.test_classinit import TestLintClassInit
 from melano.lint.fluff.test.test_docstrings import TestLintDocstrings
