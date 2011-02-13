114b016d3e2ad06097582704a80e2fabde40ccdc
Terrence Cole <terrence@zettabytestorage.com>
2011-02-12 16:44:03 -0800

    forgot this with last commit

---
+++ melano/c/pybuiltins.py
@@ -0,0 +1,20 @@
+'''
+Copyright (c) 2011, Terrence Cole.
+All rights reserved.
+'''
+
+PY_BUILTINS = {
+	'abs', 'all', 'any', 'ascii', 'bin', 'bool', 'bytearray',
+	'bytes', 'chr', 'classmethod', 'compile', 'complex',
+	'delattr', 'dict', 'dir', 'divmod', 'enumerate', 'eval',
+	'exec', 'filter', 'float', 'format', 'frozenset',
+	'getattr', 'globals', 'hasattr', 'hash', 'help', 'hex',
+	'id', 'input', 'int', 'isinstance', 'issubclass',
+	'iter', 'len', 'list', 'locals', 'map', 'max',
+	'memoryview', 'min', 'next', 'object', 'oct', 'open', 'ord',
+	'pow', 'print', 'property', 'range', 'repr', 'reversed',
+	'round', 'set', 'setattr', 'slice', 'sorted',
+	'staticmethod', 'str', 'sum', 'super', 'tuple', 'type',
+	'vars', 'zip', '__import__', '__debug__', '__doc__',
+	'__name__', '__package__', 'False', 'True', 'None'
+}
