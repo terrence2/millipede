bb421bbacdf689c0bb05ad8dbd044595ea4c8cd0
Terrence Cole <terrence@zettabytestorage.com>
2011-05-15 16:49:03 -0700

    This construct won't work, but record the syntax for later

--- data/c/env.h
+++ data/c/env.h
@@ -4,6 +4,8 @@
 #define likely(x)	__builtin_expect(!!(x), 1)
 #define unlikely(x)	__builtin_expect(!!(x), 0)
 
+#define Mp_DECREF_ALL(fst, rst...) Py_DECREF(fst); Mp_DECREF_ALL(...rst);
+
 void __init__(int argc, wchar_t **argv);
 
 void __err_capture__(char *file, int lineno, int clineno, char *context,
