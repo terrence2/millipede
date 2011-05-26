98b4caab7880ec44ba0d4527be6e54231d78bfd1
Terrence Cole <terrence@zettabytestorage.com>
2011-05-26 14:53:24 -0700

    clean up AST init to reflect our actual usage

--- melano/lang/ast.py
+++ melano/lang/ast.py
@@ -6,26 +6,26 @@ All rights reserved.
 class AST:
 	'''Base class of all ast nodes.'''
 	_fields = ()
-	__slots__ = ('symbol', 'hl', 'bb', 'bb_else', 'start', 'end') #'llnode')
+	__slots__ = ('symbol', 'hl', 'll', 'start', 'end')
 	def __init__(self, llnode):
-
-		# FIXME: push these down to block that actually use them?
-		self.bb = self.bb_else = None
-
-		#self.llnode = None
 		self.hl = None
+		self.ll = None
+
 		self.symbol = None
+
 		if llnode:
 			self.start = llnode.startpos
 			self.end = llnode.endpos
 		else:
 			self.start = self.end = None
 
+
 	def llcopy(self, other):
 		'''Take low-level parameters from the other node.'''
 		#self.llnode = other.llnode
 		self.start = other.start
 		self.end = other.end
+
 
 	def __repr__(self):
 		return '<' + self.__class__.__name__ + '>'
